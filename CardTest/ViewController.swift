c2bfda20a2702f3cc78657e2d2661157175b15e4
Jan Lübeck <jan@fyrastudio.com>
2015-09-01 11:04:05 -0400

    first commit

---
+++ CardTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CardTest
+//
+//  Created by Jan Lubeck on 9/1/15.
+//  Copyright © 2015 Fyra Studio S.A. All rights reserved.
+//
+
+import UIKit
+import CardIO //this is giving an error
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
